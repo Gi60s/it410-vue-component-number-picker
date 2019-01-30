68e060521b7191b30b45d1ec669236671dab83b8
james <james.speirs@gmail.com>
2019-01-29 18:50:41 -0700

    get picker  working

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,56 @@
 <template>
   <div id="app">
     <!-- 5. Add a number picker component with no minimum or maximum. This value ties to the "min" value -->
-    
+    <number-picker :value="min" @change="minChange">Min</number-picker>
+
     <!-- 4. Add a number picker component with minimum and maximum set from the other two pickers. This value ties to the "value" value -->
+    <number-picker :value="value" :max="max" :min="min" @change="valueChange"></number-picker>
 
     <!-- 6. Add a number picker component with no minimum or maximum. This value ties to the "max" value -->
+    <number-picker :value="max" @change="maxChange"></number-picker>
   </div>
 </template>
 
 <script>
 // 1. Import the number picker component
+import NumberPicker from './components/NumberPicker.vue'
 
 export default {
   name: 'app',
   
   components: {
     // 2. Add the number-picker component as a dependency here
-  }
+    NumberPicker
+  },
 
   // 3. Create a data object with "min" = 0, "max" = 10, and "value" = 0 
+  data () {
+    return {
+      min: 0,
+      max: 10,
+      value: 0
+    }
+  },
+
+  methods: {
+    ensureValidValue () {
+      let num = this.value
+      if (num < this.min) num = this.min
+      if (num > this.max) num = this.max
+      this.value = num
+    },
+    minChange (value) {
+      this.min = value
+      this.ensureValidValue()
+    },
+    maxChange (value) {
+      this.max = value
+      this.ensureValidValue()
+    },
+    valueChange (value) {
+      this.value = value
+    }
+  }
 }
 </script>
 <style lang="stylus">
--- src/components/NumberPicker.vue
+++ src/components/NumberPicker.vue
@@ -2,14 +2,14 @@
   <div>
     <!-- 6. Replace the standard "Picker" label a slot that allows you to place a label. The slot should have a default value of "Picker" -->
     <div>
-      Picker
+      <slot>Picker</slot>
     </div>
 
     <!-- 5. Have the buttons increment and decrement the value -->
     <div>
-      <button>-</button>
+      <button @click="decrement()">-</button>
       {{value}}
-      <button>+</button>
+      <button @click="increment()">+</button>
     </div>
   </div>
 </template>
@@ -18,12 +18,36 @@
 export default {
   name: 'number-picker',
   // 1. Add "props" to accept "value" attribute (it must be a number and defaults to zero)
-
   // 2. Add "props" to accept "min" attribute (it must be a number)
-
   // 3. Add "props" to accept "max" attribute (it must be a number)
+  props: {
+    min: Number,
+    max: Number,
+    value: {
+      type: Number,
+      default: 0
+    }
+  },
 
   // 4. Add methods to increment and decrement the value so long as the value stays within the max and min defined values
+  methods: {
+    decrement () {
+      let num = this.value - 1
+      if (this.min !== undefined && num < this.min) num = this.min
+      if (this.max !== undefined && num > this.max) num = this.max
+      this.value = num
+
+      this.$emit('change', this.value)
+    },
+    increment () {
+      let num = this.value + 1
+      if (this.min !== undefined && num < this.min) num = this.min
+      if (this.max !== undefined && num > this.max) num = this.max
+      this.value = num
+
+      this.$emit('change', this.value)
+    }
+  }
 
 }
 </script>
