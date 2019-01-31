a475bafc3ded9f1a5b90b06368672a93d83093cf
james <james.speirs@gmail.com>
2019-01-30 21:12:09 -0700

    write some tests

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,13 +1,30 @@
 import { expect } from 'chai'
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import NumberPicker from '@/components/NumberPicker.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+describe('NumberPicker.vue', () => {
+  it('has a default value of 0 if no props are put in', () => {
+    const wrapper = shallowMount(NumberPicker, {
+      // propsData: { msg }
     })
-    expect(wrapper.text()).to.include(msg)
+    expect(wrapper.text()).to.include('0')
+  })
+
+  it('overwrites the default value if value prop used', () => {
+    const wrapper = shallowMount(NumberPicker, {
+      propsData: {
+        value: 4
+      }
+    })
+    expect(wrapper.text()).to.not.include('0')
+    expect(wrapper.text()).to.include('4')
+  })
+
+  it('increments the value when + button is clicked', () => {
+    const wrapper = shallowMount(NumberPicker)
+    expect(wrapper.vm.value).to.equal(0)
+    const button = wrapper.find('button')
+    button.trigger('click')
+    expect(wrapper.vm.value).to.equal(-1)
   })
 })
